be9245a62e8fa67cfb6ee17e9fca9ae401c761e4
Romain Jobert <rjobert@student.42barcelona.com>
2023-05-11 15:29:52 +0200

    modified lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: rjobert <rjobert@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 15:07:34 by rjobert           #+#    #+#             */
-/*   Updated: 2023/05/11 15:22:06 by rjobert          ###   ########.fr       */
+/*   Updated: 2023/05/11 15:27:52 by rjobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	while (head)
 	{
 		tmp_next = head->next;
-		ft_lstdelone(head);
+		ft_lstdelone(head, del);
 		head = tmp_next;
 	}
-	head = NULL;
+	*lst = NULL;
 }
